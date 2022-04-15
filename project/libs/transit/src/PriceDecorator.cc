45000a98d009ef98eb5ed500bf9567c46884f430
kimx5154 <kimx5154@umn.edu>
2022-04-15 15:03:36 -0500

    Beeline for pythagorian theorem

--- project/libs/transit/src/PriceDecorator.cc
+++ project/libs/transit/src/PriceDecorator.cc
@@ -16,7 +16,6 @@ void PriceDecorator::setEstimatedTripPrice(Vector3 pos_, Vector3 des_) {
     float x_coor = (abs(pos_.x) - abs(des_.x)); // x_coordinate
     float y_coor = (abs(pos_.y) - abs(des_.y)); // y_coordinate
 
-    // Pythagorian theorem to get the price.
-    // e.g 10 * distance
-    estimatedPrice = 10*sqrt(pow(x_coor,2)+pow(y_coor,2)); 
+    // Pythagorian theorem to get the price for beeline
+    estimatedPrice = sqrt(pow(x_coor,2)+pow(y_coor,2));
 }
