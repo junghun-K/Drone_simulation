f6566714c100ffee3e07bf2a3031fd6b56de84a0
kimx5154 <kimx5154@umn.edu>
2022-04-15 14:49:22 -0500

    fixed error for the coordination

--- project/libs/transit/src/PriceDecorator.cc
+++ project/libs/transit/src/PriceDecorator.cc
@@ -13,8 +13,8 @@ float PriceDecorator::getEstimatedTripPrice() {
 }
 
 void PriceDecorator::setEstimatedTripPrice(Vector3 pos_, Vector3 des_) {
-    float x_coor = (pos_.x - des_.x); // x_coordinate
-    float y_coor = (pos_.y - des_.y); // y_coordinate
+    float x_coor = (abs(pos_.x) - abs(des_.x)); // x_coordinate
+    float y_coor = (abs(pos_.y) - abs(des_.y)); // y_coordinate
 
     // Pythagorian theorem to get the price.
     // e.g 10 * distance
