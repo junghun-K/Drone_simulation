7c274c982b630b9171c7d70191faadf50ef87776
Jared Grambihler <gramb025@umn.edu>
2022-04-18 09:42:12 -0500

    Only use abs on total distance

--- project/libs/transit/src/PriceDecorator.cc
+++ project/libs/transit/src/PriceDecorator.cc
@@ -13,8 +13,8 @@ float PriceDecorator::getEstimatedTripPrice() {
 }
 
 void PriceDecorator::setEstimatedTripPrice(Vector3 pos_, Vector3 des_) {
-    float x_coor = (abs(pos_.x) - abs(des_.x)); // x_coordinate
-    float y_coor = (abs(pos_.y) - abs(des_.y)); // y_coordinate
+    float x_coor = abs(pos_.x - des_.x); // x_coordinate
+    float y_coor = abs(pos_.y - des_.y); // y_coordinate
 
     // Pythagorian theorem to get the price for beeline
     estimatedPrice = sqrt(pow(x_coor,2)+pow(y_coor,2));
